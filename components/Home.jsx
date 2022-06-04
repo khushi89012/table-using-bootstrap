fe6ebb871fd6f40e26983b6451e9a687418d5fb4
khushi Gupta <95909861+khushi89012@users.noreply.github.com>
2022-06-05 00:35:27 +0530

    okk

---
+++ components/Home.jsx
@@ -0,0 +1,13 @@
+import {TableHead, TableRow} from 'material-ui/Table';
+
+export const Table = ()=>{
+    return (
+        <div>
+            <TableHead>
+  <TableRow />
+  <TableRow />
+</TableHead>
+
+        </div>
+    )
+}
